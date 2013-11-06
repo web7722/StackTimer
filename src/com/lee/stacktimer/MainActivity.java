12829a37ca5937c2ae758b4ef8e4a6cade6f684f
David Lee <cailhuiris@gmail.com>
2013-11-06 17:32:49 +0900

    Task를 추가하는 기능 구현

--- src/com/lee/stacktimer/MainActivity.java
+++ src/com/lee/stacktimer/MainActivity.java
@@ -8,9 +8,12 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.TextView;
 
 import com.lee.stacktimer.constants.AppConstant;
+import com.lee.stacktimer.data.Time;
+import com.lee.stacktimer.data.TimeStack;
 
 public class MainActivity extends Activity {
 	private BroadcastReceiver stackTimeReceiver = new BroadcastReceiver(){
@@ -34,8 +37,16 @@ public class MainActivity extends Activity {
         
         Intent intent = new Intent(this, StackTimerService.class);
         startService(intent);
-        
-        registerReceiver(stackTimeReceiver, new IntentFilter("timeStack"));
+    }
+    
+    public void addTask(View view) {
+    	TextView taskNameView = (TextView) findViewById(R.id.task);
+    	String taskName = taskNameView.getText().toString();
+    	TimeStack.INSTANCE.put(new Time(taskName));
+    }
+    
+    public void doneCurrentTask(View view){
+    	
     }
 
     @Override
